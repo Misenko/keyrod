a683691542c757ab32fd902c9009dac161691136
cduongt <cduongt@gmail.com>
2018-03-12 00:30:33 +0100

    Added params validation and ability to use CLI. The program should be fully functional now
    (cherry picked from commit d40e5d1fdd39d0ea629e1fc2832b1b65c5b522dd)

--- lib/keyrod/errors.rb
+++ lib/keyrod/errors.rb
@@ -2,5 +2,7 @@ module Keyrod
   module Errors
     class ResponseError < StandardError; end
     class ConnectionError < StandardError; end
+    class ParamsError < StandardError; end
+    class ProjectError < StandardError; end
   end
 end
